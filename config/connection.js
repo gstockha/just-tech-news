92b578341b80164bf369edb44e753c5c6f4ccca9
George Stockhammer <sunchipzzz@gmail.com>
2021-12-14 15:08:06 -0600

    added cookies, routes, handlebars

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,17 @@ require('dotenv').config();
 // create connection to our db
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) { //resources tab in heroku
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize({
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
+    port: 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
+    logging: false
   });
 }
 
